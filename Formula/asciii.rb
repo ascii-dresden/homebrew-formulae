9a5a6326d654b8749b3570e86c02ddb7f61b977e
Vincent Nadoll <me@vinado.de>
2019-12-11 14:13:53 +0100

    Update Formula to asciii version 3.9.0

--- Formula/asciii.rb
+++ Formula/asciii.rb
@@ -1,7 +1,7 @@
 class Asciii < Formula
   homepage "https://github.com/ascii-dresden/asciii"
-  url "https://github.com/ascii-dresden/asciii/releases/download/v3.6.0/asciii-v3.6.0-x86_64-apple-darwin.tar.gz"
-  sha256 "bcd2dfeb3f7bbfb75cc370ac6eac940eaecf89d1a20bb5e324e377075da3f2f0"
+  url "https://github.com/ascii-dresden/asciii/releases/download/v3.9.0/asciii-v3.9.0-x86_64-apple-darwin.tar.gz"
+  sha256 "09b4428aa556337a721448764a155df72000540b0c1cc8030e8e141ad51a4088"
 
   def install
     bin.install "asciii"
