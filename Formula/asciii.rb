bea9a95bc21907f2bf3e9b9e29c9ae4c14f025fc
Vincent Nadoll <me@vinado.de>
2020-01-14 11:46:59 +0100

    Fix package checksum

--- Formula/asciii.rb
+++ Formula/asciii.rb
@@ -1,7 +1,7 @@
 class Asciii < Formula
   homepage "https://github.com/ascii-dresden/asciii"
   url "https://github.com/ascii-dresden/asciii/releases/download/v3.10.0/asciii-v3.10.0-x86_64-apple-darwin.tar.gz"
-  sha256 "35d9de0f732e4746a7e14f000e6fc0cfa804e4809b6f5fcb5137c8d0a6ee4b27"
+  sha256 "6501a2bfabb25d57f2d29267e6b573c0d1e1faf93c7e3856d4b53ad6b17ce3ff"
 
   def install
     bin.install "asciii"
